0a9e2de178b05af7d1d78339e5941e1702852114
Paulina Dziadkiewicz <pedziadkiewicz@gmail.com>
2020-11-09 10:54:36 +0100

    Add papa parse tests

---
+++ tests/papaparse.test.js
@@ -0,0 +1,36 @@
+// eslint-disable-next-line no-global-assign
+require = require("esm")(module)
+const t = require('tape-catch')
+const Papa = require('papaparse')
+const _ = require('underscore')
+
+t.test("Papa Parse returns empty data object to Complete when step function is provided.", function(t) {
+    const csvString = "headerA,headerB\nrow1A,row1B\nrow2A,row2B"
+    const expectedParsed = [{'headerA': 'row1A', 'headerB': 'row1B'}, {'headerA': 'row2A', 'headerB': 'row2B'}]
+    let actualParsed = []
+    Papa.parse(csvString, {
+        header: true,
+        delimiter: ',',
+        step: row => {
+            actualParsed.push(row.data)
+        },
+        complete: result => {
+            t.ok(_.isEmpty(result.data))
+            t.ok(_.isEqual(actualParsed, expectedParsed))
+            t.end()
+        }
+    })
+})
+
+t.test("Papa Parse returns object to Complete with data if no step function is provided.", function(t) {
+    const csvString = "headerA,headerB\nrow1A,row1B\nrow2A,row2B"
+    const expectedParsed = [{'headerA': 'row1A', 'headerB': 'row1B'}, {'headerA': 'row2A', 'headerB': 'row2B'}]
+    Papa.parse(csvString, {
+        header: true,
+        complete: result => {
+            t.ok(_.isEqual(result.data, expectedParsed))
+            t.end()
+        }
+    })
+})
+
